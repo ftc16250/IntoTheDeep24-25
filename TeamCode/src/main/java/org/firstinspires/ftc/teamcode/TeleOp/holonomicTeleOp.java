4223579cc5d4da5e38ed8f378fc88b1cc62c790c
AMS Robotics <ddkofford@students.wcpss.net>
2024-11-21 09:42:20 -0500

    Initial Commit To ServoAlgo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/holonomicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/holonomicTeleOp.java
@@ -99,7 +99,7 @@ public class holonomicTeleOp extends OpMode {
         // -----Servo-----
 
         if (gamepad2.right_stick_y > 0) {
-            servos.setArmAngle(91);
+            servos.setArmAngle(90);
         } else if (gamepad2.right_stick_y < 0) {
             servos.setArmAngle(0);
         }
